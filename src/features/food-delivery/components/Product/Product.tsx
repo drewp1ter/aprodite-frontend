392ad77b192de81e95805d6b0cd55c0b4421fe5a
drewp1ter <belora2010@yandex.ru>
2024-04-15 11:41:13 +0300

    create compact product card

--- src/features/food-delivery/components/Product/Product.tsx
+++ src/features/food-delivery/components/Product/Product.tsx
@@ -14,35 +14,23 @@ export interface Props {
   fats?: number
   carbonhydrates?: number
   imgSrc: string
+  onCartClick?: () => void
 }
 
-export function Product(props: Props) {
+function Product(props: Props) {
   return (
     <div className={clsx(styles.product, props.className)}>
       <div className={styles.imgContainer}>
         <Image fill src={props.imgSrc} alt="" />
       </div>
+
       <div className={styles.title}>
         <h3>{props.title}</h3>
-        <b>{props.price.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB', maximumFractionDigits: 0 })}</b>
+        <b>{formatPrice(props.price)}</b>
       </div>
+
       <div className={styles.footer}>
-        <div className={styles.info}>
-          <div>
-            <small>Вес</small>
-            <small>{props.weight} Г</small>
-          </div>
-          <div>
-            <small>Ккал</small>
-            <small>{props.calories}</small>
-          </div>
-          <div>
-            <small>Б / Ж / У</small>
-            <small>
-              {props.proteins} Г / {props.fats} Г / {props.carbonhydrates} Г
-            </small>
-          </div>
-        </div>
+        <SupplementFacts {...props} className={styles.info} />
         <Button className={styles.button}>
           <CartIcon />
         </Button>
@@ -50,3 +38,50 @@ export function Product(props: Props) {
     </div>
   )
 }
+
+Product.Compact = function ProductCompact(props: Props) {
+  return (
+    <div className={clsx(styles.productCompact, props.className)}>
+      <div className={styles.imgContainerCompact}>
+        <Image fill src={props.imgSrc} alt="" />
+      </div>
+      <h3>{props.title}</h3>
+
+      <SupplementFacts {...props} className={styles.infoCompact} />
+
+      <div className={styles.footerCompact}>
+        <b>{formatPrice(props.price)}</b>
+        <Button className={styles.button}>
+          <CartIcon />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+function SupplementFacts(props: Omit<Props, 'price' | 'onCartClick' | 'title' | 'imgSrc'>) {
+  return (
+    <div className={props.className}>
+      <div>
+        <small>Вес</small>
+        <small>{props.weight} Г</small>
+      </div>
+      <div>
+        <small>Ккал</small>
+        <small>{props.calories}</small>
+      </div>
+      <div>
+        <small>Б / Ж / У</small>
+        <small>
+          {props.proteins} Г / {props.fats} Г / {props.carbonhydrates} Г
+        </small>
+      </div>
+    </div>
+  )
+}
+
+function formatPrice(price: number): string {
+  return price.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB', maximumFractionDigits: 0 })
+}
+
+export { Product }
--- src/features/food-delivery/components/ProductsPage/ProductsPage.tsx
+++ src/features/food-delivery/components/ProductsPage/ProductsPage.tsx
@@ -22,6 +22,7 @@ export function ProductsPage() {
       </div>
       <div className={styles.products}>
         <Product
+          // className={styles.productCompact}
           title="Мидии в тайском стиле"
           imgSrc={productImg.src}
           calories={476}
